9664a6972a67b1e793f6186b4399f9dc3139299b
Ramil Suleimanov <lllumineux@outlook.com>
2022-01-21 23:50:05 +0300

    Add ProjectLink model

--- src/apps/projects/admin.py
+++ src/apps/projects/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from src.apps.projects.models import Project
+from src.apps.projects.models import Project, ProjectLink
 
 admin.site.register(Project)
+admin.site.register(ProjectLink)
--- src/apps/projects/models.py
+++ src/apps/projects/models.py
@@ -24,3 +24,10 @@ class Project(models.Model):
     )
     overview_video = models.FileField(upload_to='project_overview_videos/', max_length=256, blank=True, null=True)
     is_actual = models.BooleanField(default=False)
+
+
+class ProjectLink(models.Model):
+    name = models.CharField(32)
+    icon = models.ImageField(upload_to='project_link_icons/', max_length=256)
+    link = models.CharField(256)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
