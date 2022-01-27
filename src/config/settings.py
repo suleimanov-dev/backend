53c2668ab30fe36719b35338333f1d91797c095b
Ramil Suleimanov <lllumineux@outlook.com>
2022-01-27 19:28:49 +0300

    Add MEDIA_URL to urlpatterns

--- src/config/settings.py
+++ src/config/settings.py
@@ -136,9 +136,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
-
 MEDIA_URL = 'uploads/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
