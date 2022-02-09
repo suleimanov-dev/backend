076214f0344622420736303633d2e523b183799b
Ramil Suleimanov <lllumineux@outlook.com>
2022-02-10 02:04:00 +0300

    Fix BASE_DIR

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 SECRET_KEY = os.environ.get('DB_NAME', 'dbname')
