93a3563c2e7c7f6a020edaffcd7e8f9b7785fab9
Ramil Suleimanov <lllumineux@outlook.com>
2022-02-14 18:57:07 +0300

    Rename CSRF_TRUSTED_ORIGINS to DJANGO_CSRF_TRUSTED_ORIGINS

--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -4,4 +4,4 @@ DEBUG = False
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-secret-key')
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split()
 CORS_ALLOWED_ORIGINS = os.environ.get('DJANGO_CORS_ALLOWED_ORIGINS', '').split()
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split()
+CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', '').split()
